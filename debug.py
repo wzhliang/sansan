17445273cbf8217a0f2ae10cf7f159bf9f147a7c
Wenzhi Liang <wenzhi.liang@gmail.com>
2011-09-16 12:43:48 +0100

    adding missing file. y

---
+++ debug.py
@@ -0,0 +1,7 @@
+from PyQt4.QtCore import pyqtRemoveInputHook
+from pdb import set_trace
+
+def debug_trace():
+	'''Set a tracepoint in the Python debugger that works with Qt'''
+	pyqtRemoveInputHook()
+	set_trace()
