cf62ad174d921ec9c2b4ff2a1df86a644c65ec6c
lang2 <wenzhi.liang@qq.com>
2015-03-27 14:51:49 +0800

    improvement on test code

--- sgf_test.py
+++ sgf_test.py
@@ -3,7 +3,6 @@
 import unittest
 from board import *
 from sgf import *
-from pdb import set_trace
 from pprint import pprint
 
 class GameGui(Game):
@@ -19,15 +18,15 @@ class GameGui(Game):
 		node = self.root
 		while node:
 			if node.name == "B" or node.name == "W":
-				self.goban.play_pos( node.prop, board.str2color(node.name) ) 
 				print "##############################################"
+				self.goban.play_pos( node.prop, board.str2color(node.name) ) 
 				pprint(self.goban.data)
 			else:
-				print "Non stone node:"
-				print node
+				print "Non stone node:", node
 			try:
 				node = node.children[0]
 			except IndexError:
+				print "Node has no child"
 				break
 
 	#def __getattr__(self, name):
@@ -82,25 +81,25 @@ class GameGuiA(Game):
 
 class BoardTest(unittest.TestCase):
 	def test_a(self):
-		print "\n========= a =============\n"
+		print "\n========= TEST a =============\n"
 		pan = Board()
 		game = GameGui("sgf/kj.sgf", pan)
 		game.navigate()
 
 	def test_b(self):
-		print "\n========= b =============\n"
+		print "\n========= TEST b =============\n"
 		pan = Board()
 		game = GameGui("sgf/kj.sgf", pan)
 		game.navigate_back()
 
 	def test_c(self):
-		print "\n========= c =============\n"
+		print "\n========= TEST c =============\n"
 		pan = Board()
 		game = GameGui("sgf/branch.sgf", pan)
 		game.navigate()
 
 	def test_d(self):
-		print "\n========= d =============\n"
+		print "\n========= TEST d =============\n"
 		pan = Board()
 		game = GameGui("sgf/branch.sgf", pan)
 		for i in range(5):
@@ -114,14 +113,14 @@ class BoardTest(unittest.TestCase):
 		self.assertEqual(game.where().prop, "nc")
 
 	def test_e(self):
-		print "\n========= e =============\n"
+		print "\n========= TEST e =============\n"
 		pan = Board()
 		game = GameGui("sgf/bug.sgf", pan)
 		game.navigate()
 		pprint(pan.data)
 
 	def test_f(self):
-		print "\n========= f =============\n"
+		print "\n========= TEST f =============\n"
 		pan = Board()
 		game = GameGui("sgf/yijian-Elaure.sgf", pan)
 		meta = game
@@ -134,14 +133,14 @@ class BoardTest(unittest.TestCase):
 		self.assertEqual(meta.BB, "")
 
 	def test_g(self):
-		print "\n========= g =============\n"
+		print "\n========= TEST g =============\n"
 		pan = Board()
 		game = GameGuiA("sgf/marks.sgf", pan)
 		for n in ["TR", "MA", "CR"]:
 			self.assertNotEqual(game.find_extra(n), None)
 
 	def test_h(self):
-		print "\n========= h =============\n"
+		print "\n========= TEST h =============\n"
 		print "Test support for comment in the first node as most tom qipu does"
 		pan = Board()
 		game = GameGuiA("sgf/kj1.sgf", pan)
@@ -150,7 +149,7 @@ class BoardTest(unittest.TestCase):
 		print node.get_comment()
 
 	def test_i(self):
-		print "\n========= i =============\n"
+		print "\n========= TEST i =============\n"
 		print "Test support for strange node where move property is in the middle"
 		pan = Board()
 		game = GameGuiA("sgf/strange.sgf", pan)
@@ -161,7 +160,7 @@ class BoardTest(unittest.TestCase):
 		self.assertEqual(node.prop, "pd")
 
 	def test_j(self):
-		print "\n========= j =============\n"
+		print "\n========= TEST j =============\n"
 		print "Test presentable()"
 		pan = Board()
 		game = GameGuiA("sgf/marks.sgf", pan)
@@ -177,7 +176,7 @@ class BoardTest(unittest.TestCase):
 		self.assertEqual(good, True)
 
 	def test_k(self):
-		print "\n========= k =============\n"
+		print "\n========= TEST k =============\n"
 		print "Test support for PB, PW nodes."
 		pan = Board()
 		game = GameGui("sgf/place.sgf", pan)
@@ -196,7 +195,7 @@ class BoardTest(unittest.TestCase):
 		self.assertTrue("ef" in game.where().prop)
 
 	def test_l(self):
-		print "\n========= l =============\n"
+		print "\n========= TEST l =============\n"
 		print "Test support non-std pandago property"
 		pan = Board()
 		game = GameGui("sgf/pandago.sgf", pan)
